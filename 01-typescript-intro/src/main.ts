e8fae46f1ad3eb4547e01ed9ff69390af4eb975d
Carlos Loaeza <carlosloaeza100598@gmail.com>
2025-08-27 17:21:37 -0600

    first commit

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Hola Mundo desde typescript!';
+
+console.log('Hola Mundo desde typescript!');
