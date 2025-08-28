a47db152d9a494814a658ebb139959d9e0108b63
Carlos Loaeza <carlosloaeza100598@gmail.com>
2025-08-27 18:20:03 -0600

    basic types

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+
+const name: string = 'Carlos';
+let hpPoints: number | string = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({ name, hpPoints, isAlive });
+
+export {}; // Evitar conflictos de nombres con otros archivos
